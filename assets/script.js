31f3e2000439ea5e667ef89d81bfbcd9c828960d
theRoseOfThePhoenix <sandrinebecaert@gmail.com>
2023-11-27 11:02:11 +0100

    p5 JS carousel et points animés

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,62 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let currentIndex = 0;
+
+
+const imageBanner = document.querySelector(".banner-img");
+imageBanner.src = "assets/images/slideshow/" + slides[currentIndex].image;
+
+const texte = document.querySelector(".banner-txt");
+texte.innerHTML = slides[currentIndex].tagLine;
+
+
+
+const arrowLeft = document.querySelector(".arrow_left");
+arrowLeft.addEventListener("click", function () {
+	currentIndex--;
+	//infini
+	if (currentIndex < 0) {
+		currentIndex = 3;
+	}
+	imageBanner.src = "assets/images/slideshow/" + slides[currentIndex].image;
+	texte.innerHTML = slides[currentIndex].tagLine;
+	updateSelectedDot();
+});
+
+const arrowRight = document.querySelector(".arrow_right");
+arrowRight.addEventListener("click", function () {
+	currentIndex++;
+	//infini
+	if (currentIndex > 3) {
+		currentIndex = 0;
+	}
+	imageBanner.src = "assets/images/slideshow/" + slides[currentIndex].image;
+	texte.innerHTML = slides[currentIndex].tagLine;
+	updateSelectedDot();
+});
+
+
+
+const dots = document.querySelector(".dots");
+
+const nombreElements = slides.length;
+for (let i = 0; i < slides.length; i++) {
+	const dot = document.createElement("div");
+	dot.classList.add("dot");
+	if (i === currentIndex) {
+		dot.classList.add("dot_selected");
+	} else {
+	}
+	dots.appendChild(dot);
+}
+
+//deselection des points
+function updateSelectedDot() {
+    const dotElements = dots.querySelectorAll(".dot");
+    dotElements.forEach(dot => {
+        dot.classList.remove("dot_selected");
+    });
+    dotElements[currentIndex].classList.add("dot_selected");
+}
